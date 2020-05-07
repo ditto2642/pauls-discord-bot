40cc4471c5c103c274fb98e83da203de4601e0bb
ditto2642 <ditto2642@gmail.com>
2020-05-06 18:10:48 -0700

    args dont work, testing

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const figlet = require('figlet');
 
+process.argv.forEach((t) => console.log(t));
+
 token = process.argv[2];
 console.log('Using token: ' + token);
 
